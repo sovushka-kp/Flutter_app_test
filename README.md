ee29983fa132093a6a0dc0b663f813165f4252f0
Pischanska.Kate <sovushkakp@gmail.com>
2021-09-25 15:21:55 +0300

    add change backgroud and text color

--- README.md
+++
@@ -1,2 +0,0 @@
-# Flutter_app_test
-It's my first small program with flutter&amp;dart. This program can change background color, when you click once and when you click twice change text color.
---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main()  => runApp(MyApp());
+
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  late Color _colorBac;
+  late Color _colorText;
+
+  void generateColorBac(){
+    setState(() {
+      _colorBac = Color((Random().nextDouble() * 0xFFFFFF).toInt() << 0).withOpacity(1.0);
+    });
+  }
+
+  void generateColorText() {
+    setState(() {
+      _colorText = Color.fromARGB(255, Random().nextInt(255), Random().nextInt(255), Random().nextInt(255));
+    });
+  }
+
+  @override
+  void initState() {
+    generateColorBac();
+    generateColorText();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => generateColorBac(),
+      onDoubleTap: () => generateColorText(),
+      child: Container(
+        alignment: Alignment.topCenter,
+        color: _colorBac,
+        child: Text(
+          "Click on screen",
+          style: TextStyle(fontSize: 50, color: _colorText, fontFamily: "DancingScript"),
+          textDirection: TextDirection.ltr,
+        ),
+      ),
+    );
+  }
+}
